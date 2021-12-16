329aa84cd90d3f96f9612230ad8d5ead2ad6b827
Zyy <Zyy@gmail.com>
2021-12-16 18:09:07 +0800

    更改目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+        endOfLine: 'auto' // 忽略LF、CRLF结尾符
+      }
+    ],
+    // 标签属性规则排序
+    'vue/attributes-order': 'error',
+    '@typescript-eslint/no-explicit-any': ['off']
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import 'normalize.css'
+import '@/styles/index.scss'
 
-createApp(App).use(router).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
